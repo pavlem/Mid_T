dd83481e415183709c2a42747c88c7d547ac2048
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-12-30 09:15:02 +0100

    1

---
+++ Mid_T/Views/FilterView.swift
@@ -0,0 +1,76 @@
+//
+//  FilterView.swift
+//  Mid_T
+//
+//  Created by Pavle Mijatovic on 30.12.21..
+//
+
+import SwiftUI
+
+struct FilterViewTitle: View {
+
+    let title: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct FilterView: View {
+    
+    @ObservedObject var launchesVM: LaunchesVM
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State var selectedYear = "None"
+    @State var selectedSuccessCase = "All"
+    @State var selectedOrderCase = "ASC"
+    
+    private let filterTitle = "Choose Filter Options"
+    private let launchOutcome = "Choose a launch outcome"
+    private let orederingByYear = "Select ordering by year"
+    private let successfulState = "Success"
+    private let failureStateText = "Failure"
+
+    var body: some View {
+        
+        VStack {
+            Text(filterTitle)
+                .font(.title)
+                .padding()
+            YearPickerView(selectedYear: $selectedYear, years: [selectedYear] + launchesVM.launchesByYear)
+            FilterViewTitle(title: launchOutcome)
+            LaunchSuccessView(selectedSuccessCase: $selectedSuccessCase)
+            FilterViewTitle(title: orederingByYear)
+            LaunchOrderView(selectedOrderCase: $selectedOrderCase)
+            Spacer()
+            VStack {
+                Button {
+                    launchesVM.resetFilters()
+                    
+                    let filter = LaunchesFilterObject(
+                        year: selectedYear,
+                        order: selectedOrderCase,
+                        successfulState: selectedSuccessCase,
+                        successfulStateText: successfulState,
+                        failureStateText: failureStateText
+                    )
+                    
+                    launchesVM.filter(filterObject: filter)
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text(LaunchesVM.applyFiltersText)
+                }
+                Button {
+                    launchesVM.resetFilters()
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Text(LaunchesVM.resetFiltersText)
+                }.padding()
+            }
+        }
+    }
+}
